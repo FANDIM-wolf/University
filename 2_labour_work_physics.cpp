b0a3dd78f8e1ae7e2eabf74e1bd8921075cf5d76
Mikhail Shushkovsky <63235817+FANDIM-wolf@users.noreply.github.com>
2022-10-29 15:19:30 +0300

    Add files via upload

--- 2_labour_work_physics.cpp
+++ 2_labour_work_physics.cpp
@@ -36,7 +36,7 @@ double calculate_translation_energy_D( double translation_energy_Delta , double 
 double calculate_rotational_energy(double h,double J0, double m , double R , double t , double d){
  //transform values
  h  = h * pow(10,-3);
- J0  = J0 * pow(10,-2);
+ J0  = J0 * pow(10,-3);
  m =  m * pow(10,-3);
  R = R * pow(10,-3);
  d = d * pow(10,-3);
@@ -52,7 +52,7 @@ double calculate_rotational_energy_Delta(double h,double J0, double m , double R
     double rotational_energy_Delta_first_part_of_formula= (Dh/h)+(Dt/t)+(Dd/d); // values without transformation
     //transform values
     h  = h * pow(10,-3);
-    J0  = J0 * pow(10,-2);
+    J0  = J0 * pow(10,-3);
     m =  m * pow(10,-3);
     R = R * pow(10,-3);
     d = d * pow(10,-3);
@@ -85,6 +85,38 @@ double calculate_coefficient_of_friction_D(double calculate_coefficient_of_frict
 
     return  calculate_coefficient_of_friction * calculate_coefficient_of_friction_Delta;
 }
+
+//10 formula
+double calcualate_definite_translation_energy( double m2 , double h){
+    //transform values
+    h  = h * pow(10,-3);
+    m2 =  m2 * pow(10,-3);
+    return m2 * 10 * h;
+}
+//11 formula
+double calcualate_definite_translation_energy_Delta( double m2 , double h , double Dm2 , double Dh){
+
+    return  (Dm2/m2)+(0.05/10)+(Dh/h);
+}
+//12 formula
+double calcualate_definite_translation_energy_D( double definite_translation_energy , double definite_translation_energy_D){
+
+    return  definite_translation_energy * definite_translation_energy_D;
+}
+//13 formula
+double calcualate_definite_translation_energy_with_comma(double translation_energy , double rotational_energy , double coefficient_of_friction){
+
+ return translation_energy+rotational_energy+coefficient_of_friction;
+
+}
+//14 formula
+double calcualate_definite_translation_energy_with_comma_Delta(double translation_energy_Delta , double rotational_energy_Delta , double coefficient_of_friction_Delta){
+ return translation_energy_Delta + rotational_energy_Delta + coefficient_of_friction_Delta;
+}
+//15 formula
+double calcualate_definite_translation_energy_with_comma_D(double definite_translation_energy_with_comma , double definite_translation_energy_with_comma_D){
+ return definite_translation_energy_with_comma * definite_translation_energy_with_comma_D ;
+}
 int main()
 {
 
@@ -113,7 +145,7 @@ int main()
     cin>>m;
     cout<<"Delta Mass of small cylinder '(there are four in total)': ";
     cin>>Dm;
-    cout<<"Enter mass 0 '(m0)' :";
+    cout<<"Enter mass 0 '(m0)'";
     cin>>m0;
     cout<<"Enter Delta mass 0 '(m0):";
     cin>>Dm0;
@@ -148,6 +180,22 @@ int main()
     cout<<"Coefficient of friction Delta :"<<coefficient_of_friction_Delta<<endl;
     double coefficient_of_friction_D = calculate_coefficient_of_friction_D(coefficient_of_friction ,coefficient_of_friction_Delta);
     cout<<"Coefficient of friction D:" << coefficient_of_friction_D <<endl;
+    double definite_translation_energy = calcualate_definite_translation_energy(m2,h);
+    cout<<"Definite translation energy:"<<definite_translation_energy;
+    double definite_translation_energy_Delta = calcualate_definite_translation_energy_Delta(m2,h,Dm2,Dh);
+    cout<<"Definite translation energy Delta: "<<definite_translation_energy_Delta<<endl;
+    double definite_translation_energy_D = calcualate_definite_translation_energy_D(definite_translation_energy ,definite_translation_energy_Delta);
+    cout<< "Definite translation energy D "<<definite_translation_energy_D<<endl;
+
+
+    double definite_translation_energy_with_comma = calcualate_definite_translation_energy_with_comma(translation_energy,rotational_energy,coefficient_of_friction);
+    cout<< "Definite translation energy with comma:"<<definite_translation_energy_with_comma<<endl;
+
+    double definite_translation_energy_with_comma_Delta = calcualate_definite_translation_energy_with_comma_Delta(translation_energy_D , rotational_energy_D , coefficient_of_friction_D);
+    cout<< "Definite translation energy with comma Delta:"<<definite_translation_energy_Delta<<endl;
+
+    double definite_translation_energy_with_comma_D = calcualate_definite_translation_energy_with_comma_D(definite_translation_energy_with_comma  ,definite_translation_energy_with_comma_Delta);
+    cout<< "Definite translation energy with comma D"<<definite_translation_energy_with_comma_D<<endl;
 
 
     return 0;
